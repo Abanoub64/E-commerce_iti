f58d53d139bfc4cf835b79e3be4527a5b0f13c9e
Abanoub64 <abanoubmilad64@gmail.com>
2025-07-30 01:16:06 +0300

    Added product schema

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
+
 const express = require("express");
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
@@ -6,20 +9,19 @@ const mongoose = require("mongoose");
 app.use(express.json());
 
 // Basic Route
-
 app.get("/", (req, res) => {
   res.send("E-commerce backend is running...");
 });
+app.use(authRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  .connect(process.env.MONGO_URI, {})
+  .then(() => {
+    console.log("MongoDB connected ");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
   })
-  .then(() => console.log("MongoDB connected "))
   .catch((err) => console.error("MongoDB connection error ", err));
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
