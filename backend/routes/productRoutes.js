22724ea3b612a64125ddff764bb43b9f6593914a
Muhamed Nasr <legendnasr@gmail.com>
2025-07-31 17:02:15 +0300

    product is completed

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,22 +9,21 @@ router.get("/products", async (req, res) => {
   try {
     const products = await product.find();
     res.json(products);
-    res.send("Products sent");
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 //Getting One
-router.get("/:id", productController.getproduct, async (req, res) => {
-  res.json(res.product);
+router.get("/products/:id", productController.getproduct, async (req, res) => {
   try {
-    res.status(200).json({ message: `Get product with ID ${req.params.id}` });
+    res.status(200).json(res.product);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
+
 //Creating One
-router.post("/products", productController.getproduct, async (req, res) => {
+router.post("/products", async (req, res) => {
   const { name, price, seller } = req.body;
   try {
     const newProduct = await product.create({
@@ -35,39 +34,32 @@ router.post("/products", productController.getproduct, async (req, res) => {
       image: "test3",
     });
     res.send(newProduct);
-    // res.status(201).json({ message: `${newProduct}` });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 });
 //Updating One
-router.post("/products/:id", productController.getproduct, async (req, res) => {
-  if (req.body.name != null) {
-    res.product.name = req.body.name;
-  }
-  if (req.body.email != null) {
-    res.product.email = req.body.email;
-  }
-  if (req.body.seller != null) {
-    res.product.seller = req.body.seller;
-  }
+router.put("/products/:id", productController.getproduct, async (req, res) => {
+  if (req.body.name != null) res.product.name = req.body.name;
+  if (req.body.seller != null) res.product.seller = req.body.seller;
+  if (req.body.price != null) res.product.price = req.body.price;
+
   try {
-    const updatedproduct = await res.product.save();
-    res.json(updatedproduct);
-    res
-      .status(200)
-      .json({ message: `product with ID ${req.params.id} updated` });
+    const updatedProduct = await res.product.save();
+    res.status(200).json({
+      message: "Product updated successfully",
+      data: updatedProduct,
+    });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 });
+
 //Deleting One
-router.delete("/:id", productController.getproduct, async (req, res) => {
+router.delete("/products/:id", productController.getproduct, async (req, res) => {
   try {
-    await res.product.remove();
-    res
-      .status(200)
-      .json({ message: `product with ID ${req.params.id} deleted` });
+    await res.product.deleteOne(); 
+    res.status(200).json({ message: `Product with ID ${req.params.id} deleted` });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
