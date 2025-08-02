059160b2471bc8eb2447a6d1271cc2662b646f3a
Muhamed Nasr <legendnasr@gmail.com>
2025-08-02 20:32:32 +0300

    order complete

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -13,13 +13,34 @@ module.exports.getorder = async (req, res, next) => {
       return res.status(404).json({ message: "Order not found" });
     }
 
-    res.order = getOrder;
-
-    res.order.productId.name;
-    res.order.userId.userName;
-
-    next();
+    res.json({
+      orderId: getOrder._id,
+      productName: getOrder.productId.name,
+      userName: getOrder.userId.userName,
+      totalAmount: getOrder.totalAmount,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
+
+//  <script>
+//     fetch("http://localhost:3000/api/orders")
+//       .then(res => res.json())
+//       .then(data => {
+//         const container = document.getElementById("orders-container");
+
+//         data.forEach(order => {
+//           const card = document.createElement("div");
+//           card.className = "card";
+//           card.innerHTML = `
+//             <h3>Product: ${order.productId?.name || "N/A"}</h3>
+//             <p>User: ${order.userId?.userName || "N/A"}</p>
+//             <p>Amount: ${order.totalAmount}</p>
+//           `;
+//           container.appendChild(card);
+//         });
+//       })
+//       .catch(err => console.error("Error:", err));
+//   </script>
