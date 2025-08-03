76cb4f5421e1a06c9448d55fe0a479b016e55f68
Abanoub Milad <abanoubmilad64@gmail.com>
2025-08-03 03:04:01 +0300

    Vercel config

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://127.0.0.1:5500",
+    origin: "https://e-commerce-iti-orcin.vercel.app",
     credentials: true,
   })
 );
