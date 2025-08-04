d6f3da71ee19b492dfe542f48b6d848246a386f6
Muhamed Nasr <legendnasr@gmail.com>
2025-08-04 12:52:53 +0300

    12.52

--- public/pages/admin/js/admin.js
+++ public/pages/admin/js/admin.js
@@ -84,8 +84,8 @@ async function loadOrders() {
   data.forEach((order) => {
     ordersTable.innerHTML += `
       <tr>
-        <td>${order.userId?.userName || "User"}</td>
-        <td>${order.productId?.name || "Product"}</td>
+        <td>${order.userId || "User"}</td>
+        <td>${order.productId || "Product"}</td>
         <td>${order.orderStatus}</td>
         <td><button class="btn btn-success btn-sm" onclick="confirmOrder('${
           order._id
